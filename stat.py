e82e44b84bc17e8c860317c1457bec884d0004d0
JM Iringan <iringan.jaymark14@gmail.com>
2025-09-27 20:40:27 +0800

    first

--- stat.py
+++ stat.py
@@ -2,3 +2,13 @@ print("APPDEVLAB ~ Activity 2".center(50, "="))
 print("\033[94m=" *50)
 print("\033[91mStudent Grading and Scholarship Eligibility System")
 print("\033[94m=" * 50)
+
+def get_academic_average(average):
+    if average >= 96:
+        return "With Highest Honor"
+    elif average >= 90:
+        return "With Honors"
+    elif average >= 75:
+        return "Passed"
+    else:
+        return "Failed"
